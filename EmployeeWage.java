bb3fb457513942d58468a17fa98f364fc23a3a89
dvranga <devangmranganth>
2020-03-31 22:13:15 +0530

    [ranganath]Added: employee wage calculating for 20 days

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,23 +3,33 @@ public class EmployeeWage
 	public static final int FULL_TIME=1;
 	public static final int PART_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_OF_WORK_DAYS=20;
 	public static void main(String[] args) {
+		
 		int empHrs=0;
 		int empWage=0;
-		int value= (int) (Math.floor(Math.random()*10)%3);
-		switch (value)
+		int totalWage=0;
+		for(int days=1;days<=NUM_OF_WORK_DAYS;days++)
 		{
-			case FULL_TIME:
-				empHrs=8;
-				break;
-			case PART_TIME:
-				empHrs=4;
-				break;
-			default:
-				empHrs=0;
+			int value= (int) (Math.floor(Math.random()*10)%3);
+			switch (value)
+			{
+				case FULL_TIME:
+					empHrs=8;
+					break;
+				case PART_TIME:
+					empHrs=4;
+					break;
+				default:
+					empHrs=0;
+			
+			}
+			empWage=empHrs*EMP_RATE_PER_HOUR;
+			//System.out.println(empWage+"emp dailywage");
+			totalWage+=empWage;
+			//System.out.println(totalWage+"wage for "+days);
 		}
-		empWage=empHrs*EMP_RATE_PER_HOUR;
-		System.out.println(empWage);
+		System.out.println("total employee Wage is "+totalWage);
 	}
 }
 
