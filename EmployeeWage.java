1922f0e1a921a097bfba187deae074d15f219849
dvranga <devangmranganth>
2020-03-31 21:57:53 +0530

    [ranganath]Added: using Swith case Employee wage assume 20 rupees and half day is 4hrs

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,16 +1,25 @@
 public class EmployeeWage
 {
+	public static final int FULL_TIME=1;
+	public static final int PART_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
 	public static void main(String[] args) {
-		int FullTime=1;
-		double value= Math.floor(Math.random()*10)%2;
-		if(value==FullTime)
+		int empHrs=0;
+		int empWage=0;
+		int value= (int) (Math.floor(Math.random()*10)%3);
+		switch (value)
 		{
-			System.out.println("employee is present");
+			case FULL_TIME:
+				empHrs=8;
+				break;
+			case PART_TIME:
+				empHrs=4;
+				break;
+			default:
+				empHrs=0;
 		}
-		else
-		{
-			System.out.println("employee is Absent");
-		}
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println(empWage);
 	}
 }
 
