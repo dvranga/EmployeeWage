65a179eb905c3e5fad1dcd4dff23ca7a18f6802a
dvranga <devangmranganth>
2020-04-01 15:07:44 +0530

    [ranganath]Refactor:calculating the total Employee Wage by initilize the variables through constructer in different class

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,9 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class EmployeeWage
 {
-	public static String COMPANY_NAME;
+	public  String COMPANY_NAME;
 	int EMP_RATE_PER_HOUR;
 	int MAX_OF_WORK_DAYS;
 	int MAX_WORK_HRS_IN_MONTH;
@@ -9,25 +12,18 @@ public class EmployeeWage
      public static final int PART_TIME=2;
 	public EmployeeWage(String COMPANY_NAME, int EMP_RATE_PER_HOUR, int MAX_OF_WORK_DAYS, int MAX_WORK_HRS_IN_MONTH) {
 		super();
-		EmployeeWage.COMPANY_NAME = COMPANY_NAME;
+		this.COMPANY_NAME = COMPANY_NAME;
 		this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
 		this.MAX_OF_WORK_DAYS = MAX_OF_WORK_DAYS;
 		this.MAX_WORK_HRS_IN_MONTH = MAX_WORK_HRS_IN_MONTH;
 	}
-	public static void main(String[] args) {
-		EmployeeWage emp=new EmployeeWage("Dmart",20, 20,100 );
-		int  totalWage=emp.getTotalWage("Dmart",20,20,100);
-        System.out.println("total employee Wage"+COMPANY_NAME + " is "+totalWage);
-        totalWage=emp.getTotalWage("Relance",50,25,150);
-        System.out.println("total employee Wage"+COMPANY_NAME+" comany is "+totalWage);
-	}
-	private  int getTotalWage(String COMPANY_NAME, int EMP_RATE_PER_HOUR, int MAX_OF_WORK_DAYS, int MAX_WORK_HRS_IN_MONTH) {
+	
+	public int getTotalWage(String COMPANY_NAME, int EMP_RATE_PER_HOUR, int MAX_OF_WORK_DAYS, int MAX_WORK_HRS_IN_MONTH) {
 		System.out.println("calculating the employeewage of company "+COMPANY_NAME); 
 		 int empWage=0;
          int totalWage=0;
          int totalEmpHrs=0;
          int workingDays=1;
-         
 		  while(totalEmpHrs<=MAX_WORK_HRS_IN_MONTH && workingDays<MAX_OF_WORK_DAYS)
           {
                  empHrs=getHrs();
@@ -39,6 +35,9 @@ public class EmployeeWage
           }
 		return totalWage;
 	}
+	
+	
+
 	private static int getHrs() {
 		int value= (int) (Math.floor(Math.random()*10)%3);
         switch (value)
@@ -53,6 +52,42 @@ public class EmployeeWage
                         empHrs=0;
         }
 		return empHrs;
+	}
+	@Override
+	public String toString() {
+		return "EmployeeWage [EMP_RATE_PER_HOUR=" + EMP_RATE_PER_HOUR + ", MAX_OF_WORK_DAYS=" + MAX_OF_WORK_DAYS
+				+ ", MAX_WORK_HRS_IN_MONTH=" + MAX_WORK_HRS_IN_MONTH + "]";
+	}
+
+}
+
+
+ class Companies {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner =new Scanner (System.in);
+		System.out.println("enter how many companies data you want to find");
+		int number=scanner.nextInt();
+        HashMap<String,Integer> hm=new HashMap<>();
+       for(int i=0;i<number;i++)
+       {
+    	   System.out.println("enter company name");
+    	   String companyName=scanner.next();
+    	   System.out.println("enter the rate per hour");
+    	   int ratePerHour=scanner.nextInt();
+    	   System.out.println("enter the workingdays in month");
+    	   int workingDaysInMonth=scanner.nextInt();
+    	   System.out.println("enter the total work Hours");
+    	   int totalWorkHours=scanner.nextInt();
+    	   
+    	   EmployeeWage emp=new EmployeeWage(companyName,ratePerHour, workingDaysInMonth,totalWorkHours );
+    	   int value= emp.getTotalWage(companyName, ratePerHour, workingDaysInMonth,totalWorkHours);
+    	   hm.put(companyName, value);
+       }
+       System.out.println("total wage of companies");
+       System.out.println(hm); 
+       scanner.close();
 	}
 }
 
